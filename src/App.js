6c81d0c42d3fe946961a1ebd74e1a718823fcc86
yanuarprayoga09 <yanuarprayoga@gmail.com>
2023-05-03 19:28:03 +0700

    warnain baju

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ class App extends React.Component{
   render(){
     return (
       <>
-        <div className="container">
+        <div className="container py-5">
           <div className="row">
-            <div className="col-12">
+            <div className="col-12 text-center">
               <h1>FEEDUITEN APPS</h1>
               <h4>Rp. 1.500.000,-</h4>
               <span className="title">Sisa uang kamu tersisa 75% lagi</span >
               <hr />
             </div>
           </div>
-          <div className="row">
+          <div className="row ">
             <div className="col-6">
               <div className="card-wrapper">
                 <div className="icon-wrapper">
-                  <i className="bi bi-wallet"></i>
+                  <i className="bi bi-wallet p-1 rounded"></i>
                 </div>
               <span className="title">pemasukan</span>
               <h3>Rp. 2.000.000,-</h3>
@@ -31,7 +31,7 @@ class App extends React.Component{
             <div className="col-6">
               <div className="card-wrapper">
                 <div className="icon-wrapper">
-                  <i className="bi bi-wallet"></i>
+                  <i className="bi bi-wallet p-1 rounded"></i>
                 </div>
               <span className="title">pemasukan</span>
               <h3>Rp. 2.000.000,-</h3>
@@ -39,23 +39,25 @@ class App extends React.Component{
               </div>
             </div>
             
-            <div className="row">
+            <div className="row py-5">
               <div className="col-12 d-flex justify-content-between align-items-center">
-                <h2>Ringkasan Transaksi</h2>
-                <div className="wrapper-button">
-                  <button>Pemasukan <i class="bi bi-plus-circle"></i></button>
+                <h2 >Ringkasan Transaksi</h2>
+                <div className="button-wrp d-flex">
+                <div className="wrapper-button px-2">
+                  <button className="btn btn-primary">Pemasukan <i class="bi bi-plus-circle"></i></button>
                 </div>
                 <div className="wrapper-button">
-                  <button>Pengeluaran <i class="bi bi-dash-lg"></i></button>
+                  <button className="btn btn-danger">Pengeluaran <i class="bi bi-dash-lg"></i></button>
+                </div>
                 </div>
               </div>
             </div>
 
-            <div className="row">
-              <div className="row-12 d-flex justify-content-between">
+            <div className="row bg-primary rounded text-light ">
+              <div className="row-12 d-flex justify-content-between align-items-center">
                 <div className="d-flex">
-                <div className="icon-wrapper d-flex align-items-center">
-                <i className="bi bi-wallet"></i>
+                <div className="icon-wrapper d-flex align-items-center px-3">
+                <i className="bi bi-wallet p-1 rounded"></i>
                 </div>
                 <div className="transaction">
                   <h6>Menerima gaji</h6>
